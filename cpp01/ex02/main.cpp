8c79bc88c33f2e691fa367052ad15b46f53c46a7
loswaldo <loswaldo@student.21-school.ru>
2021-06-30 18:58:11 +0300

    create ptr & ref to string

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	std::string value = "HI THIS IS BRAIN";
+	std::string *valuePtr = &value;
+	std::string &valueRef = value;
+
+	std::cout << "POINTER : " << *valuePtr << std::endl;
+	std::cout << "REFERENCE : " << valueRef << std::endl;
+}
